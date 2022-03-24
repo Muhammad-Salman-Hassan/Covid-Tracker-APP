e48968e07af0d6e76792f9fa046f6ef5f73b9146
Muhammad-Salman-Hassan <msalmanhassan288311@gmail.com>
2022-03-24 10:01:28 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 
+// import { Class } from '@mui/icons-material';
+import React from 'react'
+// import React, { useEffect, useState } from 'react';
+import { fetchData } from './api';
 import './App.css';
 import Header from './components/Header';
 import MainGrid from './components/MainGrid';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <MainGrid/>
-    </div>
-  );
-}
 
-export default App;
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data })
+  }
+
+  
+
+  render() {
+    // const {data}=this.state
+    
+    return (
+      <div className="App">
+      <Header/>
+      <MainGrid data={this.state.data}/>
+    </div>
+    );
+  }
+}
+export default App
+
--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -1,7 +1,8 @@
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import GlobalData from './GlobalData';
+// import GlobalData from './GlobalData';
+import { Typography } from '@mui/material';
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -12,14 +13,49 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-export default function MainGrid() {
+export default function MainGrid({data:{confirmed , deaths, recovered, lastUpdate}}) {
+  console.log(confirmed.value)
+  if(!confirmed){
+    return <div>looding....</div> 
+  }
   return (
     <Box sx={{ width: 1,marginTop:4}}>
       <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1} sx={{ marginLeft:5,marginRight:5}}>
         
         <Box gridColumn="span 3">
         <Item sx={{backgroundColor:"white",border:"none",boxShadow:"none"}}>
-            <GlobalData/>
+        <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        "& > :not(style)": {
+          m: 1,
+          width: "100%",
+          height: 128,
+        },
+      }}
+    >
+      
+      <Paper elevation={3}>
+        Active
+        <Typography variant="h3" component="h2" sx={{marginTop:"5px"}}>
+          {/* {confirmed.value} */}
+        </Typography>
+      </Paper>
+      <Paper elevation={3}>
+        {" "}
+        Recovered
+        <Typography variant="h3" component="h2" sx={{marginTop:"5px"}}>
+          1000
+        </Typography>
+      </Paper>
+      <Paper elevation={3}>
+        Fatatlitites
+        <Typography variant="h3" component="h2" sx={{marginTop:"5px"}}>
+          {/* {deaths.value} */}
+        </Typography>
+      </Paper>
+    </Box>
         </Item>
         </Box>
         <Box gridColumn="span 9">
